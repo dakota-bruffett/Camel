c70f0023f8c98712f003bd53de97d5452780ca17
nq2623up <DakotaBruffett@gmail.com>
2023-09-09 12:06:12 -0500

    camel

--- main.py
+++ main.py
@@ -7,15 +7,21 @@
 def camelcase(sentence):
     title_case = sentence.title()
     upper_title_case = title_case.replace(''," ")
-
+    #Here is where the sentences are made for camel case
     return upper_title_case[1:0].lower() + upper_title_case[1:]
 
 def banner():
     message = 'Amazing camel sentence'
     star = '*'* len(message)
     print(f'\n{star} \n{message} \n{star}')
+    # this part up here is the where we make the banner for the camel project
+
+def Insturtions():
+    print('please print the message in camelcase ')
+    #Here is the instructions for the camel case
 def main():
     sentence = input('enter a sentence')
+    # here is where you input the sentence
     output = camelcase(sentence)
     print(output)
 
